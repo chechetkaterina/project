fbcf79ea6af8a34e27e38554343a1baf221f0c46
Ekaterina <chechet.katerina@mail.ru>
2022-07-20 14:17:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = + prompt("Вам есть 18?", "18");
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[0] = promt('Как ваше имя?', '');
+//answers[1] = promt('Как ваша фамилия?', '');
+//answers[2] = promt('Сколько вам лет?', '');
+
+//console.log(typeof (answers));
+//console.log(typeof (null));
+
+const category = 'toys';
+
+console.log(`htts://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
